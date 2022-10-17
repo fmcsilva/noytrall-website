b1d85a9dc6fe8cdcbc721e57c0497170a4aafd12
Francisco Silva <fmcsilva00@gmail.com>
2022-10-17 15:24:21 +0100

    updated footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -36,6 +36,114 @@ const Footer: React.FC = () => {
             <div>
               <ul className="uk-list uk-list-collapse">
                 <li>
+                  <a className="" href="-7aboutus">
+                    About us
+                  </a>
+                </li>
+                <li>
+                  <a className="" href="-7privacy">
+                    Privacy Policy
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <strong>noytral technologies</strong>
+              <br />
+              Rua Pedro Nunes
+              <br />
+              3030-199 Coimbra
+            </div>
+            <div>
+              <div>
+                <a href="#">
+                  <i className="lab la-lg la-facebook-f"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-twitter"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-instagram"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-youtube"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-whatsapp"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-medium-m"></i>
+                </a>
+                <a href="#">
+                  <i className="lab la-lg la-linkedin-in"></i>
+                </a>
+              </div>
+              <div>
+                <a className="" href="info@noytrall.pt">
+                  info@noytrall.pt
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="uk-margin-large" data-uk-grid>
+            <div>
+              <img
+                style={{ background: "none" }}
+                data-src={iapmei}
+                width="100"
+                height=""
+                alt="iapmei"
+                data-uk-img
+              />
+            </div>
+            <div>
+              <img
+                style={{ background: "none" }}
+                data-src={portugal2020}
+                width="350"
+                height=""
+                alt="portugal2020"
+                data-uk-img
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+
+  return (
+    <footer id="footer">
+      <div className="uk-section uk-section-secondary">
+        <div className="uk-container uk-container-large">
+          <div className="uk-child-width-1-4@m uk-grid-margin" data-uk-grid>
+            <div>
+              <ul className="uk-list uk-list-collapse">
+                <li>
+                  <a className="" href="/">
+                    For Guests
+                  </a>
+                </li>
+                <li>
+                  <a className="" href="/hotels">
+                    For Hotels
+                  </a>
+                </li>
+                <li>
+                  <a className="" href="/providers">
+                    For Providers
+                  </a>
+                </li>
+                <li>
+                  <a className="" href="/help">
+                    Help and Support
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div>
+              <ul className="uk-list uk-list-collapse">
+                <li>
                   <a className="" href="/aboutus">
                     About us
                   </a>
