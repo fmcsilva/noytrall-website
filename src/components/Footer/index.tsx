75af7beac5ba81a5ec815254297739cb3d42957d
Francisco Silva <fmcsilva00@gmail.com>
2022-10-25 11:17:27 +0100

    new footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,22 +12,22 @@ const Footer: React.FC = () => {
             <div>
               <ul className="uk-list uk-list-collapse">
                 <li>
-                  <a className="" href="/">
+                  <a className="" href="index.htm">
                     For Guests
                   </a>
                 </li>
                 <li>
-                  <a className="" href="/hotels">
+                  <a className="" href="hotels.htm">
                     For Hotels
                   </a>
                 </li>
                 <li>
-                  <a className="" href="/providers">
+                  <a className="" href="providers.htm">
                     For Providers
                   </a>
                 </li>
                 <li>
-                  <a className="" href="/help">
+                  <a className="" href="help.htm">
                     Help and Support
                   </a>
                 </li>
@@ -36,12 +36,17 @@ const Footer: React.FC = () => {
             <div>
               <ul className="uk-list uk-list-collapse">
                 <li>
-                  <a className="" href="-7aboutus">
+                  <a className="" href="aboutus.htm">
                     About us
                   </a>
                 </li>
                 <li>
-                  <a className="" href="-7privacy">
+                  <a className="" href="mission.htm">
+                    Mission and Values
+                  </a>
+                </li>
+                <li>
+                  <a className="" href="privacy.htm">
                     Privacy Policy
                   </a>
                 </li>
@@ -89,113 +94,6 @@ const Footer: React.FC = () => {
             <div>
               <img
                 style={{ background: "none" }}
-                data-src={iapmei}
-                width="100"
-                height=""
-                alt="iapmei"
-                data-uk-img
-              />
-            </div>
-            <div>
-              <img
-                style={{ background: "none" }}
-                data-src={portugal2020}
-                width="350"
-                height=""
-                alt="portugal2020"
-                data-uk-img
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-
-  return (
-    <footer id="footer">
-      <div className="uk-section uk-section-secondary">
-        <div className="uk-container uk-container-large">
-          <div className="uk-child-width-1-4@m uk-grid-margin" data-uk-grid>
-            <div>
-              <ul className="uk-list uk-list-collapse">
-                <li>
-                  <a className="" href="/">
-                    For Guests
-                  </a>
-                </li>
-                <li>
-                  <a className="" href="/hotels">
-                    For Hotels
-                  </a>
-                </li>
-                <li>
-                  <a className="" href="/providers">
-                    For Providers
-                  </a>
-                </li>
-                <li>
-                  <a className="" href="/help">
-                    Help and Support
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <ul className="uk-list uk-list-collapse">
-                <li>
-                  <a className="" href="/aboutus">
-                    About us
-                  </a>
-                </li>
-                <li>
-                  <a className="" href="/privacy">
-                    Privacy Policy
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div>
-              <strong>noytral technologies</strong>
-              <br />
-              Rua Pedro Nunes
-              <br />
-              3030-199 Coimbra
-            </div>
-            <div>
-              <div>
-                <a href="#">
-                  <i className="lab la-lg la-facebook-f"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-twitter"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-instagram"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-youtube"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-whatsapp"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-medium-m"></i>
-                </a>
-                <a href="#">
-                  <i className="lab la-lg la-linkedin-in"></i>
-                </a>
-              </div>
-              <div>
-                <a className="" href="info@noytrall.pt">
-                  info@noytrall.pt
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="uk-margin-large" data-uk-grid>
-            <div>
-              <img
                 data-src={iapmei}
                 width="100"
                 height=""
